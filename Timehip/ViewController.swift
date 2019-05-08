1aee116e358d70156c5dd4fcd892157b3ca29843
Detlef Wormstall <dwormstall@mac.com>
2019-05-08 11:42:06 +0200

    Initial Commit

---
+++ Timehip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timehip
+//
+//  Created by Wormstall on 08.05.19.
+//  Copyright © 2019 Wormstall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
