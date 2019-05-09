08784140bfc0d41d955d8df431c8931b40483212
Detlef Wormstall <dwormstall@mac.com>
2019-05-09 22:32:51 +0200

    List with datasource and delegates

---
+++ Timehip/TimeHipViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Timehip
+//
+//  Created by Wormstall on 08.05.19.
+//  Copyright © 2019 Wormstall. All rights reserved.
+//
+
+import UIKit
+
+class TimeHipViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Dragon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+
+    //Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
