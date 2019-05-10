b154fe267fc8304c6d49033c78582e1f34bff998
Detlef Wormstall <dwormstall@mac.com>
2019-05-10 17:47:49 +0200

    Add new item function added

--- Timehip/TimeHipViewController.swift
+++ Timehip/TimeHipViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TimeHipViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Dragon"]
+    var itemArray = ["Example entry one", "Example entry 2"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,5 +45,24 @@ class TimeHipViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    //Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new TimeHip item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
