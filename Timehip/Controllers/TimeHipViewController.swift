bccfcf1d3d2a3c412396d7cd8eb0ee78eb848ce5
Detlef Wormstall <dwormstall@mac.com>
2019-05-23 16:14:13 +0200

    Convert to MVC

--- Timehip/Controllers/TimeHipViewController.swift
+++ Timehip/Controllers/TimeHipViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class TimeHipViewController: UITableViewController {
     
-    var itemArray = ["Example entry one", "Example entry 2"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ListArray") as? [String] {
+        
+        let newItem = Item()
+        newItem.title = "Example 1"
+        itemArray.append(newItem)
+        
+        
+        if let items = defaults.array(forKey: "ListArray") as? [Item] {
             itemArray = items
         }
     }
@@ -29,7 +35,11 @@ class TimeHipViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -39,11 +49,9 @@ class TimeHipViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -55,7 +63,10 @@ class TimeHipViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new TimeHip item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "ListArray")
             self.tableView.reloadData()
         }
---
+++ Timehip/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Timehip
+//
+//  Created by Wormstall on 17.05.19.
+//  Copyright © 2019 Wormstall. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
